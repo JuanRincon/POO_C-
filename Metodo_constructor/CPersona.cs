eb11850d3be2a57466c8b8ff9c2e042460fa39d8
Curum <Curum@DESKTOP-OJS0JKN>
2024-04-21 13:26:09 -0500

    Metodo constructor

---
+++ Metodo_constructor/CPersona.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metodo_constructor
+{
+    class CPersona
+    {
+        private string nombre;
+        private int edad;
+
+        public CPersona()
+        {
+            Console.WriteLine("Estamos en el constructor");
+            nombre = "";
+            edad = 0;
+        }
+
+        public string Nombre
+        {
+            set
+            {
+                nombre = value;
+            }
+        }
+
+        public int Edad
+        {
+            set
+            {
+                edad = value;
+            }
+        }
+
+        public void muestra()
+        {
+            Console.WriteLine("Nombre: {0}, Edad: {1}", nombre, edad);
+        }
+      
+    }
+}
