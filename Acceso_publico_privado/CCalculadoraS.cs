7f18cb653017e71bf31f024f77f105f950a9be6a
Curum <Curum@DESKTOP-OJS0JKN>
2024-04-23 14:57:13 -0500

    Metodo estatico - incluido en carpeta Acceso_publico_privado

---
+++ Acceso_publico_privado/CCalculadoraS.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metodos_estaticos
+{
+    class CCalculadoraS
+    {
+        public static double suma(double a, double b)
+        {
+            return a + b;
+        }
+
+        public static double resta(double a, double b)
+        {
+            return a - b;
+        }
+
+        public void saludo()
+        {
+            Console.WriteLine("Hola");
+        }
+    }
+}
