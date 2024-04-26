c501179d63e06585f6cde9b106e5c9bc9f76690d
juan <Curum@DESKTOP-OJS0JKN>
2024-04-26 12:17:23 -0500

    Herencia de clase base o padre a subclase o clase hijo

--- Metodo_constructor/CPersona.cs
+++ Metodo_constructor/CPersona.cs
@@ -4,33 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Metodo_constructor
+namespace Herencia
 {
     class CPersona
     {
         private string nombre;
         private int edad;
-
-        public CPersona()
-        {
-            string tempEdad = "";
-
-            Console.WriteLine("Estamos en el cosntructor");
-            Console.WriteLine("Dame el nombre");
-            nombre = Console.ReadLine();
-            Console.WriteLine("Dame la edad");
-            tempEdad = Console.ReadLine();
-            edad = Convert.ToInt32(tempEdad);
-        }
-
-        // Sobrecarga
-        public CPersona(string pNombre, int pEdad)
-        {
-            Console.WriteLine("Estamso en el constructor sobrecargado");
-
-            nombre = pNombre;
-            edad = pEdad;
-        }
 
         public string Nombre
         {
@@ -38,33 +17,37 @@ namespace Metodo_constructor
             {
                 nombre = value;
             }
-        }
+
+            get
+            {
+                return nombre;
+            }
+        }  
 
         public int Edad
         {
             set
             {
-                edad = value;
+                if (value >= 18)
+                {
+                    edad = value;
+                }
+                else
+                {
+                    edad = -1;
+                    Console.WriteLine("Edad no permitida");
+                }
+            }
+
+            get
+            {
+                return edad;
             }
         }
 
-        public void muestra()
+        public void Muestra()
         {
             Console.WriteLine("Nombre: {0}, Edad: {1}", nombre, edad);
         }
-
-        // Método destructor
-        // Nos permite garantizar un la forma correcta del cierre del objeto
-        // Haciendo liberación de mejoria, se puede desde aca cerrar archivos,
-        // verificar que se haya desconectado correctamente de la red,
-        // limpiar los valores de las variables por seguridad
-        ~CPersona()   // Método destructor        
-        {
-            Console.WriteLine("Adios desde {0}", nombre);
-            // Limpiamos información
-            nombre = "";
-            edad = 0;
-        }
-      
     }
 }
--- Metodo_constructor/Program.cs
+++ Metodo_constructor/Program.cs
@@ -4,19 +4,44 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Metodo_constructor
+namespace Herencia
 {
     class Program
     {
         static void Main(string[] args)
         {
-            CPersona p1 = new CPersona();
+            // Creamos objeto de la clase persona
+            CPersona uno = new CPersona();
+            uno.Nombre = "Maria";
+            uno.Edad = 27;
 
-            CPersona p2 = new CPersona("Juan", 27);
+            uno.Muestra();
+            Console.WriteLine("--------");
 
-            p1.muestra();
-            p2.muestra();
+            // Creamos un objeto d la clase Empleado
+            CEmpleado dos = new CEmpleado();
 
+            // Colocamos la información usando las propiedades de su clase base
+            dos.Nombre = "Juan";
+            dos.Edad = 30;
+
+            dos.Puesto = "Contador";
+            dos.Salario = 10500.50;
+
+            // Usamos la muestra de empleado
+            dos.EmpleadoMuestra();
+
+            // Verificamos que podemos usar la muestra de la clase base
+            dos.Muestra();
+
+            Console.WriteLine("--------");
+
+            // Creamos otro empleado
+            CEmpleado tres = new CEmpleado();
+
+            tres.ponerDatos("Susana", 15, "Programadora", 150000.30);
+
+            tres.EmpleadoMuestra();
         }
     }
 }
