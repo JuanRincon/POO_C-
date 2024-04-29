decb3c1e672b6177b782dbae9062f28c269c4092
juan <Curum@DESKTOP-OJS0JKN>
2024-04-29 11:24:01 -0500

    Agregacion

---
+++ Agregacion/CEdificio.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agregacion
+{
+    class CEdificio
+    {
+        private string direccion;
+        private int CantDepartamentos;
+        private int DepRentados;
+
+        public CEdificio(string pDireccon, int pCantidad, int pRentados)
+        {
+            direccion = pDireccon;
+            CantDepartamentos = pCantidad;
+            DepRentados = pRentados;
+        }
+
+        public void Muestra()
+        {
+            Console.WriteLine("Edificio en {0} con {1} departamentos, {2} rentados", direccion, CantDepartamentos, DepRentados);
+        }
+    }
+}
---
+++ Agregacion/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agregacion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ejemplo de agregación
+
+            // Creamos al propietario
+
+            CPropietario Juan = new CPropietario("Juan");
+
+            // Creamos la propiedad
+
+            CEdificio edif1 = new CEdificio("Av. 1 num 56", 10, 3);
+
+            // Vemos info del propietario
+
+            Juan.MostrarPropiedad();
+
+            // Le colocamos la propiedad
+
+            Juan.AdicionaPropiedad(edif1);
+            Juan.MostrarPropiedad();
+
+            // Eliminamos a Juan
+            Juan = null;         // Luego de eliminar el contenido de Juan
+            GC.Collect();      // GC.Collect borra todos los datos nulos
+            // Juan.MostrarPropiedad(); por lo que al ejecutar el objeto Juan sale un error
+
+            Console.WriteLine("-----");
+
+            // Verificamos que el edificio exista independiente de Juan
+            edif1.Muestra();
+        }
+    }
+}
