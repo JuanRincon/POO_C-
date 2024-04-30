40e48a1f2ac7b33494994d8115560a61dfbf528e
juan <Curum@DESKTOP-OJS0JKN>
2024-04-29 23:06:55 -0500

    Funciones virtuales y override de una función o método

---
+++ FuncionesVirtuales_overRride_Sealed/CProductoImportado.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FuncionesVirtuales_overRride_Sealed
+{
+    class CProductoImportado:CProducto
+    {
+        protected double impuesto;
+        
+        public CProductoImportado(string pDescripcion, double pPrecioCompra, double pImpuesto)
+            : base(pDescripcion, pPrecioCompra)
+        {
+            impuesto = pImpuesto;
+        }
+
+        public override void CalculaPrecio()
+        {
+            Console.WriteLine("Calcula precio de importado");
+            precioVenta = (precioCompra * (1 + impuesto)) * 1.30;
+        }
+    }
+}
