3cb74dc2c09cd5c598deb1be21ef77f7c2b53f3a
juan <Curum@DESKTOP-OJS0JKN>
2024-05-02 12:11:32 -0500

    Polimorfismo

---
+++ Polimorfismo/CAnimal.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polimorfismo
+{
+    class CAnimal
+    {
+        protected string nombre;
+
+        public string Nombre { get { return nombre; } set { nombre = value; } }
+
+        public virtual void Moverse()
+        {
+            Console.WriteLine("El animal se mueve");
+        }
+    }
+}
---
+++ Polimorfismo/CAve.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polimorfismo
+{
+    class CAve:CAnimal
+    {
+        public override void Moverse()
+        {
+            Console.WriteLine("El ave {0} vuela", nombre);
+        }
+    }
+}
